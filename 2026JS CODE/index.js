aa7211b1b105cfe54f3db4dc3518452db26a8b27
Megha706765 <meghathakur2003123@gmail.com>
2026-03-07 07:44:42 +0530

    JSCode

---
+++ 2026JS CODE/index.js
@@ -0,0 +1,25 @@
+console.log("Enter the 1 to 6 number are print :")
+for(let i=1; i<=6; i++){
+    console.log(i+" ");
+}
+console.log("Print reverse number :");
+for(let j=6; j>=1; j--){
+    console.log(j+" ");
+}
+let age = 13;
+if(age>=18){
+    console.log("You can vote");
+    console.log("You can drive");
+}
+if(age<18){
+    console.log("You cannot vote");
+}
+let firstName = "Megha";
+console.log(`Welcome ${firstName}`);
+
+let age2child = 26;
+if(age2child < 18){
+    console.log("You cannot vote");
+}else{
+    console.log("you can vote");
+}
